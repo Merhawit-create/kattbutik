43a03c1c6ed5b095e5d96a475621e66ea651affc
Merhawit Mebrahtu <merhawitristet431@gmail.com>
2026-05-21 02:34:46 +0200

    Add cart state using Context API and  home and owner information pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <section>
+      <h1>Om ägaren</h1>
+      <p>Namn: Yodit</p>
+      <p>E-post: yodit.rm@gmail.com</p>
+      <p>Adress: Stockholm, Sverige</p>
+    </section>
+  );
+}
+
+export default About;
