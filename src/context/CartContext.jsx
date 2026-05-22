e5c6234b8f4ad941165c9c5610cb66b944dc1dad
Merhawit Mebrahtu <merhawitristet431@gmail.com>
2026-05-22 03:28:36 +0200

    Add comments to cart context

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,14 +1,15 @@
 import { createContext, useContext, useState } from "react";
-
+// Create global cart context
 const CartContext = createContext();
 
 export function CartProvider({ children }) {
+   // Store all cats added to cart
   const [cartItems, setCartItems] = useState([]);
-
+ // Add selected cat to cart
   function addToCart(cat) {
     setCartItems((prev) => [...prev, cat]);
   }
-
+  // Remove all items after order is completed
   function clearCart() {
     setCartItems([]);
   }
@@ -19,7 +20,7 @@ export function CartProvider({ children }) {
     </CartContext.Provider>
   );
 }
-
+// Custom hook for easier cart access
 export function useCart() {
   return useContext(CartContext);
 }
